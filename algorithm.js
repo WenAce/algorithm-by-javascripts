64bd8d2da2161a8340ae04876bc639a78d740ae4
wenace <luowenfeng60925@163.com>
2017-03-02 10:09:52 +0800

    new add

--- algorithm.js
+++ algorithm.js
@@ -29,3 +29,76 @@ function QuickSort(arr){
    }
    return left.QuickSort().concat(mid.concat(right.QuickSort()));
 }
+
+<!--二路归并-->
+function merageSort(left,right){
+  var result=[];
+  var i=0,j=0;
+  while (i<left.length && j<right.length) {
+    if (left[i]<right[j]) {
+       result.push(left[i++]);
+    }else {
+      result.push(right[j++]);
+    }
+  }
+
+  while (i<left.length) {
+    result.push(left[i++]);
+  }
+  while (j<right.length) {
+    result.push(right[j++]);
+  }
+  return result;
+}
+
+<!--选择排序-->
+function selectSort(arr){
+  var length=arr.length,
+      indexMin;
+
+  for (var i = 0; i < length-1; i++) {
+      indexMin=i;
+      for(var j=i;j<length;j++){
+        if (arr[indexMin]>arr[j]) {
+          indexMin=j;
+        }
+      }
+      if (i!==indexMin) {
+        var temp=0;
+        temp=i;
+        i=indexMin;
+        indexMin=temp;
+      }
+  }
+}
+
+<!--sequentialSearth-->
+function sequentialSearth(arr,item){
+  var length=arr.length;
+  for(var i=0;i<length;i++){
+    if(item===arr[i])
+    return i;
+  }
+  return -1;
+}
+
+<!--binarySearch-->
+function binarySearch(arr,item){
+  arr.QuickSort();
+  var low=0,
+      high=arr.length-1,
+      mid,element;
+
+    while (low<=high) {
+        mid=Math.floor((low+high)/2);
+        element=arr[mid];
+        if(element<item)
+          low=mid+1;
+          else if(element>item)
+          high=mid-1;
+          else {
+            return mid;
+          }
+    }
+    return -1;
+}
